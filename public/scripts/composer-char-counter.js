2b31664dc78dc47499ac2870ff2427a4774f2db2
Suliman Fayyaz <hydrajin@gmail.com>
2021-12-19 19:12:04 -0500

    Add shadow and icon hover color for tweet skeleton

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,63 +1,19 @@
-// document.addEventListener("dblclick", (e) => {
-//   let cursorX = e.pageX;
-//   let cursorY  = e.pageY;
-//   alert(document.innerHTML = `x: ${cursorX}, y:  ${cursorY}`);
-// });
-
 //! jQuery
-// A $( document ).ready() block.
-$(document).ready(function() {
-  // console.log("ready!");
-  // addEventListener("keypress", (event) => {
-  //   console.log(event);
 
-  // });
+$(document).ready(function() {
 
   //* Use input for textarea
 
-  // any input
   $("#tweet-text").on("input", function() {
-    // console.log(this);
-    // this referes to the tweet-text id of the counter
-    // const charInput = this.value.length;
-    const charInput = $(this).val().length;
-    // if (charInput < 140) {
-    //   // text.css('color', 'red');
-    //   console.log("DUDE");
-    // }
-    const remainingChar = 140;
-    const charCounter = $("output.counter");
-    // console.log(charCounter);
 
-    const limit = remainingChar - charInput;
-    charCounter.text(limit);
-    // if all 140 characters are used up
-    limit < 0 ?
-    // change counter to css class limit which is red
-      $("output.counter").addClass("limit") :
-    // else remove class if 140 characters aren't used
-      $("output.counter").removeClass("limit");
+    const text = this.value.length;
+    const remainingChar = 140;
+    const updatedCounter = remainingChar - text;
+    
+    $(".counter").html(updatedCounter);
+
+    updatedCounter < 0 ?
+      $(".counter").addClass("limit") :
+      $(".counter").removeClass("limit");
   });
 });
-
-// if (limit < 0) {
-//   // Find other method to use!
-//   $("output.counter").addClass("limit");
-//   // console.log("LIMIT EXCEEDED");
-// } else {
-//   $("output.counter").removeClass("limit");
-// }
-// });
-// });
-
-
-//     // if all 140 characters are used up
-//     if (limit < 0) {
-//       // Find other method to use!
-//       charCounter.css("color", "#DC143C");
-//       // console.log("LIMIT EXCEEDED");
-//     } else {
-//       charCounter.css("color", "#6B6B6B");
-//     }
-//   });
-// });
