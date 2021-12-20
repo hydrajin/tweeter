ed1a28a802ca11f36174fbc17d483859d4bc2aa8
Suliman Fayyaz <hydrajin@gmail.com>
2021-12-20 02:05:06 -0500

    add JQUERY test function to view tweets.JSON data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,23 +4,22 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function() {
-// Test / driver code (temporary). Eventually will get this from the server.
-// const tweetData = {
-//   "user": {
-//     "name": "Newton",
-//     "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@SirIsaac"
-//     },
-//   "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//   "created_at": 1461116232227
-// };
+$(document).ready(function () {
+  // Test / driver code (temporary). Eventually will get this from the server.
+  // const tweetData = {
+  //   "user": {
+  //     "name": "Newton",
+  //     "avatars": "https://i.imgur.com/73hZDYK.png",
+  //       "handle": "@SirIsaac"
+  //     },
+  //   "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //   "created_at": 1461116232227
+  // };
 
-const createTweetElement = (tweet) => {
-  const $tweet = 
-  $(`<article class="tweet-box tweet-hov">
+  const createTweetElement = (tweet) => {
+    const $tweet = $(`<article class="tweet-box tweet-hov">
   <section class="header-2">
     <div class="profile-detail">
       <div class="imagename">
@@ -44,75 +43,66 @@ ${tweet.content.text}
   </footer>
 </article>`);
 
-return $tweet;
-};
+    return $tweet;
+  };
 
-// const $tweet = createTweetElement(tweetData);
+  // const $tweet = createTweetElement(tweetData);
 
-// // Test / driver code (temporary)
-// console.log($tweet); // to see what it looks like
-// const existingTweets = $('#tweet-container');
-// existingTweets.prepend($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  // // Test / driver code (temporary)
+  // console.log($tweet); // to see what it looks like
+  // const existingTweets = $('#tweet-container');
+  // existingTweets.prepend($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 
+  // // Fake data taken from initial-tweets.json
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd" },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   },
+  //   {
+  //     "user": {
+  //     "name": "Vernon Weber",
+  //     "handle": "@Weber",
+  //     "avatars": "https://i.imgur.com/2WZtOD6.png"
+  //     },
+  //     "content": {
+  //     "text": "adsdasdas"
+  //     },
+  //     "created_at": 1639967862034
+  //     },
+  // ]
 
+  $.getJSON("http://localhost:8080/tweets", function (data) {
+    //! I used this JQUERY  function (Andy AJAX lecture 00:10:00 to test if tweets.JSON data would work)
+    // So far, all the data loads and tweets work when page is refreshed... 
 
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-    "name": "Vernon Weber",
-    "handle": "@Weber",
-    "avatars": "https://i.imgur.com/2WZtOD6.png"
-    },
-    "content": {
-    "text": "adsdasdas"
-    },
-    "created_at": 1639967862034
-    },
-]
-
-const renderTweets = function(tweets) {
-// loops through tweets
-  for (const tweet of tweets) {
-  // calls createTweetElement for each tweet
-  const $newTweet = createTweetElement(tweet);
-// takes return value and appends it to the tweets container
-$("#tweet-container").prepend($newTweet);
-
-
-
-// // const createTweetElement = function(tweet) {
-// // let $tweet = /* Your code for creating the tweet element */
-// // // ...
-// return $tweet;
-// }
-// }
-  }
-}
-renderTweets(data);
-
-
+    const renderTweets = function (tweets) {
+      // loops through tweets
+      for (const tweet of tweets) {
+        // calls createTweetElement for each tweet
+        const $newTweet = createTweetElement(tweet);
+        // takes return value and appends it to the tweets container
+        $("#tweet-container").prepend($newTweet);
+      }
+    };
+    renderTweets(data);
+  });
 });
-
